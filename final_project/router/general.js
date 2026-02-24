658c4eed5fe14e052fd3fc30400cd9201c2fda86
Gabby Huerta <gihuerta3147@gmail.com>
2026-02-24 15:07:13 -0500

    added Promises for tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -25,28 +25,62 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+    
+    new Promise((resolve,reject) => {
+        resolve(books);
+    })
+    .then(data => {
+        res.send(JSON.stringify(data,null,4));
+    })
+    .catch(err => {
+        res.send("Error retrieving books!");
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book) {
-    res.json(book);
 
-  } else {
-    res.send("Unable to find book!");
-  }
+    const isbn = req.params.isbn;
+    new Promise((resolve,reject) => {
+        const book = books[isbn];
+
+        if (book) {
+            resolve(book);
+        } else {
+            reject("Unable to find book!");
+        }
+    })
+    .then(book => {
+        res.send(book);
+    })
+    .catch(err => {
+        res.send(err);
+    });
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+
     const author = req.params.author;
-    const filtered_books = Object.values(books).filter(
-        book => book.author === author
-      );
-    res.send(filtered_books);
+    new Promise((resolve,reject) => {
+        const filtered_books = Object.values(books).filter(
+            book => book.author === author
+        );
+
+        if (filtered_books.length > 0) {
+            resolve(filtered_books);
+        } else {
+            reject("No books found for this author!");
+        }
+    })
+    .then(data => {
+        res.send(JSON.stringify(data));
+    })
+    .catch(err => {
+        res.send(err);
+    });
+    
 });
 
 // Get all books based on title
@@ -61,12 +95,20 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    const book = books[isbn];
+    new Promise((resolve,reject) => {
+        const book = books[isbn];
     if (book) {
-        res.send(book.reviews);
+        resolve(book.reviews);
     } else {
-        res.send("Unable to find book!");
+        reject("Unable to find book!");
     }
+    })
+    .then(reviews => {
+        res.send(JSON.stringify(reviews));
+    })
+    .catch(err => {
+        res.send(err);
+    });
 });
 
 module.exports.general = public_users;
